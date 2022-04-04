f4d07c56e16b540b9fff286ba52a68e93ba626f8
Damian Brito <brito314@gmail.com>
2022-04-03 22:57:46 -0400

    Finalized Influx component. Subject to change to support other types of dbs.

--- Influx.py
+++ Influx.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 from influxdb_client import InfluxDBClient, Point, WritePrecision
-from influxdb_client.client.write_api import ASYNCHRONOUS
+from influxdb_client.client.write_api import ASYNCHRONOUS, SYNCHRONOUS
 from nMapper import Mapper
 from nSpeed import Speeder
 
@@ -29,11 +29,23 @@ class Influx:
                 vendor = dev[1]['vendor']
             try:
                 for port in dev[1]['ports']:
-                    query = f"devices,ip={ip},host={hostname},mac={mac},os={dev_os},vendor={vendor} port={port}"
-                    sequence.append(query)
+                    point = Point("devices") \
+                        .tag("ip", f"{ip}") \
+                        .tag("host", f"{hostname}") \
+                        .tag("mac", f"{mac}") \
+                        .field("os", f"{dev_os}") \
+                        .field("vendor", f"{vendor}") \
+                        .field("port", port)
+                    sequence.append(point)
             except TypeError:
-                query = f"devices,ip={ip},host={hostname},mac={mac},os={dev_os},vendor={vendor} port=''"
-                sequence.append(query)
+                point = Point("devices") \
+                    .tag("ip", f"{ip}") \
+                    .tag("host", f"{hostname}") \
+                    .tag("mac", f"{mac}") \
+                    .field("os", f"{dev_os}") \
+                    .field("vendor", f"{vendor}") \
+                    .field("port", -1)
+                sequence.append(point)
         return sequence
 
     def get_port_data(self):
@@ -47,27 +59,45 @@ class Influx:
     def get_curr_net_data(self):
         data = self.speeder.run()
         hosts = self.mapper.get_sum_netstats()
-        tf = ["speeds",
-              f"source={data['source_ip']}",
-              f"download={data['download']}, "
-              f"upload={data['upload']}",
-              f"ping={data['ping']}",
-              f"bytes_sent={data['bytes_sent']}",
-              f"bytes_received={data['bytes_recv']}",
-              f"uphosts={hosts['uphosts']}",
-              f"available_ips={hosts['downhosts']}",
-              f"total_ips={hosts['totalhosts']}"]
-        fields = ",".join(tf[2:])
-        query = f"{tf[0]},{tf[1]} {fields}"
-        return query
+        # tf = ["speeds",
+        #       f"source={data['source_ip']}",
+        #       f"download={data['download']}, "
+        #       f"upload={data['upload']}",
+        #       f"ping={data['ping']}",
+        #       f"bytes_sent={data['bytes_sent']}",
+        #       f"bytes_received={data['bytes_recv']}",
+        #       f"uphosts={hosts['uphosts']}",
+        #       f"available_ips={hosts['downhosts']}",
+        #       f"total_ips={hosts['totalhosts']}"]
+        # fields = ",".join(tf[2:])
+        # query = f"{tf[0]},{tf[1]} {fields}"
+        point = Point("speeds") \
+            .tag("source", data['source_ip']) \
+            .field("download", data['download']) \
+            .field("upload", data['upload']) \
+            .field("ping", data['ping']) \
+            .field("tx", data['bytes_sent']) \
+            .field("rx", data['bytes_recv']) \
+            .field("uphosts", hosts['uphosts']) \
+            .field("available_ips", hosts['downhosts']) \
+            .field("total_ips", hosts['totalhosts'])
+        return point
 
-
-# with InfluxDBClient(url="http://localhost:8086", token=token, org=org) as client:
-#     write_api = client.write_api(write_options=ASYNCHRONOUS)
-#
-#     data = "test_bucket,host=host1 used_percent=23.43234543"
-#     data = "test_bucket,host=host1 used_percent=23.43234543"
-#     write_api.write(bucket, org, data)
-
-
-# client.close()
+    def update_influx(self):
+        print("[!] Writing to InfluxDB")
+        with InfluxDBClient(url="http://localhost:8086", token=self.token, org=self.org) as client:
+            write_api = client.write_api(write_options=SYNCHRONOUS)
+            device_bat = self.get_device_data()
+            ports_bat = self.get_port_data()
+            curr_stats = self.get_curr_net_data()
+            for data_set in [device_bat, ports_bat, curr_stats]:
+                for data in data_set:
+                    print(data.to_line_protocol())
+                    write_api.write(self.bucket, self.org, data)
+            # print(" [+] Devices updated!")
+            # write_api.write(self.bucket, self.org, ports_bat)
+            # print(" [+] Ports updated!")
+            # write_api.write(self.bucket, self.org, curr_stats)
+            # print(" [+] Network status updated!")
+        print("[+] Write to db completed!")
+        client.close()
